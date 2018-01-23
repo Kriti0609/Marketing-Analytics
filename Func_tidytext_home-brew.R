8a8828fc5eb5fb22c72da4f996e8827948c03dd6
Kriti0609 <35517756+Kriti0609@users.noreply.github.com>
2018-01-23 17:43:54 +0530

    Update Func_tidytext_home-brew.R

--- Func_tidytext_home-brew.R
+++ Func_tidytext_home-brew.R
@@ -24,8 +24,9 @@ clean_corpus<-function(text,user_stopwords){
   words<-unique(all_words)
   stopword_df=data.frame(words)
   text_df=data_frame(text=text)
-  textdf_doc = text_df %>% mutate(doc = seq(1:nrow(text_df))) %>% group_by(doc)
-  textdf_doc1=textdf_doc %>% unnest_tokens(words,text) %>% count(words, sort = FALSE) %>% rename(count = n)
+  #textdf_doc = text_df %>% mutate(doc = seq(1:nrow(text_df))) %>% group_by(doc)
+  textdf_doc1=textdf_doc %>% unnest_tokens(words,text) 
+  #%>% count(words, sort = FALSE) %>% rename(count = n)
   textdf_final= anti_join(textdf_doc1,stopword_df,by="words")
   
   return (textdf_final)
